43d23958e42a9d2afa87d1e0c134ed43b7259b11
Leozin22 <96589644+Leozin22@users.noreply.github.com>
2022-03-21 19:35:20 -0300

    acrescentei mais variaveis

--- teste.c
+++ teste.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 main(){
 	int x, y, z=10;
-	y=2;
-	x=3;
+	float a=3, c=4.23, d;
+	char i, j= 'a';
+	scanf ("%d %d %f %c", &x,&y,&d,&i);
 	printf("x= %d, y= %d, z= %d", x,y,z);
-	
+	printf ("\n");
+	printf ("a= %f,c= %f,d=%f", a, c, d);
+	printf ("\n");
+	printf ("i=%c e j=%c", i,j);
 	getch();
 	//nao pode ser colocado a variavel apos o codigo
 }
