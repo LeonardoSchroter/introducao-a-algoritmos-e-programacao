8b5691a8b3507824a38c1f03c01d681068ebeb24
Leozin22 <96589644+Leozin22@users.noreply.github.com>
2022-03-21 20:44:36 -0300

    Update teste2.c

--- teste2.c
+++ teste2.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 main(){
 	int x;
+	float y;
+	char h;
 	printf("digite um numero \n");
 	scanf("%i", &x);
-	printf("o valor digitado para x é igual a , %i ", x);
-	
+	fflush(stdin);
+	printf("o valor digitado para x e igual a , %i\n ", x);
+	scanf("%f", &y);
+	fflush(stdin);
+	printf("o valor digitado para y e igual a , %f \n", y);
+	scanf("%c", &h);
+	fflush(stdin);
+	printf("o valor digitado para h e igual a , %c ", h);
+	//o fflush limpa os buffs, é obrigatorio depois de character
+
+	getch();
 	
 }
 
