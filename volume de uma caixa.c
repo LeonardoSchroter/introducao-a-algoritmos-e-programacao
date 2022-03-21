f7220e7dd99886ac1022a4275d21522fd2384d86
Leozin22 <96589644+Leozin22@users.noreply.github.com>
2022-03-21 20:59:36 -0300

    Update volume de uma caixa.c

--- volume de uma caixa.c
+++ volume de uma caixa.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 main(){
+	//1.     Desenvolva um algoritmo para ler as medidas de uma caixa, calcular e mostrar o seu volume.
 	float c,a,l,v;
 	
 	printf("digite os valores do comprimento, altura e largura de uma caixa, respectivamente \n");
@@ -9,7 +10,8 @@ main(){
 	
 	v=c*a*l;
 	
-	printf("o volume é igual a %f",v);
+	printf("o volume e igual a %f",v);
+	getch();
 }
 
 
