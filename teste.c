a2602750bb16e8f0598e724617b73f02fe3d3b3f
Leozin22 <96589644+Leozin22@users.noreply.github.com>
2022-03-21 19:50:12 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 main(){
+	#define pi 3.1415
 	int x, y, z=10;
 	float a=3, c=4.23, d;
 	char i, j= 'a';
+	printf("o pi=%f\n",pi); 
 	scanf ("%d %d %f %c", &x,&y,&d,&i);
 	printf("x= %d, y= %d, z= %d", x,y,z);
 	printf ("\n");
