72394c134a13d9268952ce111529bbd0b8aa9946
Leozin22 <96589644+Leozin22@users.noreply.github.com>
2022-03-21 21:55:39 -0300

    Create eleicoes.c

---
+++ eleicoes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main(){
+	//6.Escreva um algoritmo para ler o número de eleitores de um município, o número de votos brancos, nulos e válidos. Calcule e escreva o percentual que cada um representa em relação ao total de eleitores.
+	
+	int e,vv,vn,vb,pv,pn,pb;
+	
+	printf("qual foi o numero de eleitores?");
+	scanf("%i",&e);
+	printf("qual foi o numero de votos validos, votos em branco e votos nulos, respectivamente?");
+	scanf("%i",&vv);
+	scanf("%i",&vb);
+	scanf("%i",&vn);
+	pb=(100*pb)/e;
+	pv=(100*pv)/e;
+	pn=(100*pn)/e;
+	printf()
+		
+	
+	
+	
+	
+	
+	
+	getch();
+}
