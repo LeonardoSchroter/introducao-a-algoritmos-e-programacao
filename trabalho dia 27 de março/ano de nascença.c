8b90ee1f59316a8fde531e34972e2816a7b49992
Leozin22 <leo.schroter@gmail.com>
2022-04-04 15:26:57 -0300

    Create ano de nascença.c

---
+++ trabalho dia 27 de março/ano de nascença.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+  main(){
+  	
+  	//4 - Escreva um algoritmo que leia a idade de uma pessoa, e então mostre na tela o ano em que ela nasceu.
+  	int a, i;
+  	
+  	printf("qual a sua idade?\n");
+  	scanf("%i ", &i);
+  
+  	a=2022-i;
+  	
+  	printf("voce tem %d anos", a);
+  	
+  	
+	  
+	  
+	  
+	  getch();
+  }
