90a4baca9e3c39ffef36e977928bcd69803fb2c2
Leonardo Barros <96589644+Leozin22@users.noreply.github.com>
2022-04-25 18:35:37 -0300

    Update TIMES.c

--- aula do dia 1804/TIMES.c
+++ aula do dia 1804/TIMES.c
@@ -1,12 +1,12 @@
 /*6) Escreva um algoritmo que leia o placar de um jogo da primeira fase da copa do Brasil. Caso o time de fora tenha ganho 
-o jogo por mais de 2 gols de diferença, mostre na tela uma mensagem indicando que o time de fora já se classificou para a 
-próxima fase. Caso contrário, mostre uma mensagem indicando que os dois times irão se enfrentar novamente em um novo jogo.
+o jogo por mais de 2 gols de diferen?a, mostre na tela uma mensagem indicando que o time de fora j? se classificou para a 
+pr?xima fase. Caso contr?rio, mostre uma mensagem indicando que os dois times ir?o se enfrentar novamente em um novo jogo.
 
-ex: time da casa 4 x 3 time de fora, mostra "Os dois times se enfrentarão em um novo jogo"
+ex: time da casa 4 x 3 time de fora, mostra "Os dois times se enfrentar?o em um novo jogo"
 
-ex2: time da casa 1 x 3 time de fora, mostra "O time de fora já se classificou"
+ex2: time da casa 1 x 3 time de fora, mostra "O time de fora j? se classificou"
 
-plus: caso ocorra um segundo jogo, leia o placar desse novo jogo e então diga quem passou de fase.*/
+plus: caso ocorra um segundo jogo, leia o placar desse novo jogo e ent?o diga quem passou de fase.*/
 #include<stdio.h>
 main(){
 	int tf,tc;
@@ -19,7 +19,7 @@ main(){
 	}
 	else {
 		printf("os times se enfrentarao novamente\n");
-	}
+	
 	printf("escreva o placar do novo jogo,primeiramente inserindo quantos gols fez o time de casa\n");
 	scanf("%d",&tc);
 	scanf("%d",&tf);
@@ -27,10 +27,10 @@ main(){
 		printf("o time de casa ganhou\n");
 	}
 	 if(tf>tc){
-		printf("o time de casa ganhou\n");
+		printf("o time de fora ganhou\n");
 			
 		}
-	
+	}
 	
 	
 	
