10df825f39dc497cdc4971478c391433d2805d17
Leozin22 <leo.schroter@gmail.com>
2022-05-17 12:10:55 -0300

    exercicios

---
+++ trabalho dia 16 de maio/exercicio 10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+/*10) Faça um algoritmo para calcular e mostrar o resultado dos 50 primeiros elementos da série:
+
+
+1000   -   997   +   994   -   991   +   ...
+
+  1            2             3           4
+  */
+  main(){
+  	
+  	
+  }
