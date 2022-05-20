2afdf3873db1459b91c945f13769b365ed6a0c5d
Leozin22 <leo.schroter@gmail.com>
2022-05-20 13:25:21 -0300

    feito

--- trabalho dia 16 de maio/exercicio 10.c
+++ trabalho dia 16 de maio/exercicio 10.c
@@ -8,6 +8,20 @@
   1            2             3           4
   */
   main(){
-  	
+  	int i;
+  	float num,resultado;
+  	resultado=0;
+  	num=1000;
+  	for (i=1;i<=50;i++){
+  		if(i %2!=0){
+           resultado += num/i;
+	   }
+	    else{
+	       resultado-=num/i;
+	   	}
+	   	num-=3;
+	
+	}
+	printf("o resultado eh %.2f",resultado);
   	
   }
