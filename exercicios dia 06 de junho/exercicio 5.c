e950e74acd68dbfd01df0d1df005ec9a457bd796
Leozin22 <leo.schroter@gmail.com>
2022-06-11 15:42:57 -0300

    confirmado

---
+++ exercicios dia 06 de junho/exercicio 5.c
@@ -0,0 +1,11 @@
+
+/*5 - Fazer um algoritmo que leia vinte números inteiros armazenando-os em um vetor, a seguir, 
+troque o conteúdo da primeira posição do vetor com o conteúdo da décima primeira posição, 
+o conteúdo da segunda posição com o da décima segunda, e assim, sucessivamente até trocar o conteúdo da décima 
+com o da vigésima e escreva o vetor após estas modificações.*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main(){
+	
+}
