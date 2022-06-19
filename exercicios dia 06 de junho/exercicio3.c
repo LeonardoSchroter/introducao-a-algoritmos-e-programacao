c3dfbd886f20ca690788e8eb2e2ec2205a3ff147
Leozin22 <leo.schroter@gmail.com>
2022-06-19 17:27:05 -0300

    a

--- exercicios dia 06 de junho/exercicio3.c
+++ exercicios dia 06 de junho/exercicio3.c
@@ -1,2 +1,22 @@
 /*3 - Escreva um algoritmo que leia os valores para um vetor de 10 elementos, e em seguida ordene em ordem crescente os valores desse vetor, 
 utilizando um vetor auxiliar.*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main(){
+	int vetora[10],vetorb[10],j, i;
+	
+	for(i=0;i<10;i++){
+		scanf("%d",&vetora[i]);
+		
+	}
+	vetorb[1]=vetora[1];
+		for(i=0;i<10;i++){
+			for(j=0,j<10,j++){
+				if(vetorb[i]<vetora[j]){
+					
+				}
+			}
+		}
+		
+}
---
+++ exercicios dia 06 de junho/exercicio8.c
@@ -0,0 +1,22 @@
+/*8 - Escreva um programa que leia uma matriz de ordem 5 (ou seja, 5x5) e verifique se a soma dos elementos da 
+diagonal principal é igual a soma dos elementos da diagonal secundária.*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main(){
+	
+	int vetor[5][5],i,j,sdp=0;
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			scanf("%d",&vetor[i][j]);
+		}
+	}
+		for(i=0;i<5;i++){
+			for(j=0;j<5;j++){
+				if (i==j){
+					sdp=+vetor[i][j];
+				}
+				if
+			}
+		}
+}
