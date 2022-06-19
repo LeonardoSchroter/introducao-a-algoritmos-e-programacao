c829e6bacc4bf1b0c80adef0cfec5debf49efc0e
Leozin22 <leo.schroter@gmail.com>
2022-06-19 19:33:47 -0300

    terminadas

--- exercicios dia 06 de junho/exercicio8.c
+++ exercicios dia 06 de junho/exercicio8.c
@@ -5,18 +5,30 @@ diagonal principal é igual a soma dos elementos da diagonal secundária.*/
 #include<math.h>
 main(){
 	
-	int vetor[5][5],i,j,sdp=0;
+	int vetor[5][5],i,j,sdp=0,sds=0;
 	for(i=0;i<5;i++){
 		for(j=0;j<5;j++){
-			scanf("%d",&vetor[i][j]);
+			scanf(" %d",&vetor[i][j]);
 		}
 	}
 		for(i=0;i<5;i++){
 			for(j=0;j<5;j++){
 				if (i==j){
-					sdp=+vetor[i][j];
+					sdp+=vetor[i][j];
+					printf("soma diagonal principal %d\n",sdp);
 				}
-				if
+				if (i==5-1-j){
+					sds+=vetor[i][j];
+						printf("soma diagonal sec %d\n",sds);
+				}
 			}
+		}
+		printf("diagonal principal é %d\n",sdp);
+		printf("diagonal secundaria é %d\n",sds);
+		if (sdp==sds){
+			printf("a soma da diagona principal e secundaria sao iguais");
+		}
+		else{
+			printf("a soma da diagonal principal e secundaria sao diferentes");
 		}
 }
---
+++ exercicios dia 06 de junho/exercicio9.c
@@ -0,0 +1,14 @@
+/*9 - Escreva um programa que leia uma matriz de ordem 5 e verifique se os elementos da diagonal principal 
+(da esquerda para a direita) são os mesmos da diagonal secundária (direita pra esquerda).*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main(){
+	
+	int vetor[5][5],i,j;
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			scanf(" %d",&vetor[i][j]);
+		}
+	}
+}
