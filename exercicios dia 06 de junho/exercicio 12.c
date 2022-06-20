9a722a8f5384f997ad2b58fc5adf2e4890db65c5
Leonardo Barros <96589644+Leozin22@users.noreply.github.com>
2022-06-20 20:49:49 -0300

    feito

--- exercicios dia 06 de junho/exercicio 12.c
+++ exercicios dia 06 de junho/exercicio 12.c
@@ -11,14 +11,21 @@ main(){
 		}
 	
 	}
-	for(i=0;i<5;i++){
-		for(j=0;j<4;j++){
-			vetor[i][j]=vetorb[j][i];
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+			vetorb[i][j]=vetor[j][i];
 		}
 	}
-	for(i=0;i<5;i++){
-		for(j=0;j<4;j++){
-			printf("[%d][%d]=%d\n",j,i,vetorb[j][i]);
+	
+	
+	
+	
+	
+	
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+			printf("[%d][%d]=%d\n",i,j,vetorb[i][j]);
+			
 		}
 	}
 }
---
+++ trabalho final/trab.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main(){
+	char palavra[40];
+	int chutes,i;
+	int erros;
+	printf("qual a palavra que vosmece deseja ?\n");
+	scanf("%s",&palavra);
+	system("cls");
+	printf("você possui 10 chances de acertar a palavra\n");
+	for(i=0;i<9)
+
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	getch();
+}
