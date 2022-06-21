2a37751784ca776e0ac12257f9fa90d67a25554e
Leonardo Barros <96589644+Leozin22@users.noreply.github.com>
2022-06-20 21:19:55 -0300

    a

--- trabalho final/trab.c
+++ trabalho final/trab.c
@@ -2,14 +2,19 @@
 #include<conio.h>
 #include<math.h>
 main(){
-	char palavra[40];
+	char palavra[TAM],j;
 	int chutes,i;
 	int erros;
 	printf("qual a palavra que vosmece deseja ?\n");
 	scanf("%s",&palavra);
 	system("cls");
 	printf("você possui 10 chances de acertar a palavra\n");
-	for(i=0;i<9)
+	for(i=0;i<9;i++){
+		for(j=0;i<TAM;i++){
+			
+		}
+	}
+	
 
 	
 
