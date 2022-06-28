3048687316e5c58e428ca004a0e240972e697504
Leozin22 <leo.schroter@gmail.com>
2022-06-27 22:15:45 -0300

    trabalho pronto

--- trabalho final/trab.c
+++ trabalho final/trab.c
@@ -3,17 +3,21 @@
 #include<math.h>
 #include<string.h>
 main(){
-	char palavra[20],letra,pal[20];
+	char palavra[20],letra,pal[20],pal2[20];
 	int chutes=5,j,i,c;
-	int erros;
+	int erros=0;
 	
 	printf("qual a palavra que vosmece deseja ?\n");
 	scanf("%s",&palavra);
+	fflush(stdin);
 	for(i=0;i<strlen(palavra);i++){
 		pal[i]= '-';
+		pal2[i]= '-';
 	}
+	
+	
 	system("cls");
-	fflush(stdin);
+	
 	
 	for(;;){
 		
@@ -21,41 +25,68 @@ main(){
 			
 			
 			printf("%c",pal[j]);
-			fflush(stdin);
+			
 		}
-		printf("\n tente a sorte\n");
+		c=0;
+		erros=0;
+		printf("\nvoce tem %d tentativas\n",chutes);
+		printf(" digite uma letra:");
 		scanf("%c",&letra); 
 		fflush(stdin);
 		for(j=0;j<strlen(palavra);j++){
 			if(letra==palavra[j]){
 				pal[j]=palavra[j];
+				palavra[j]='.';
+				c++;
+				
+				
+				}
 			}
-			else{
+			if (c==0){
 				chutes--;
+				printf("\nvoce errou\n");
 			}
 			
+			
+			
+		
+	
+		if(chutes==0){
+			printf("\n voce perdeu");
+			break;
 			
 		}
-		if(chutes==0){
+		for(j=0;j<strlen(palavra);j++){
+			if(pal2[j]!=pal[j]){
+				erros++;
+				continue;
+			}
+		
+		}
+			
+		if (erros==strlen(palavra)){
+			printf("parabens, voce acertou a palavra ");
 			break;
 		}
 		
 		
+		
 	}
-
-	
-
-	
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	getch();
+getch();	
 }
+	
+
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
